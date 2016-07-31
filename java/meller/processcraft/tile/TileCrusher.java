706a4c50fba4860026e76b48530941c3e2f85915
Meller <logger127@hotmail.com>
2016-07-31 14:32:56 +0200

    StackSlots

--- java/meller/processcraft/tile/TileCrusher.java
+++ java/meller/processcraft/tile/TileCrusher.java
@@ -22,39 +22,77 @@ public class TileCrusher extends TileEntity implements IInventory
 	}
 
 	@Override
-	public boolean hasCustomName() {
-		// TODO Auto-generated method stub
+	public boolean hasCustomName() 
+	{
 		return false;
 	}
 
 	@Override
-	public int getSizeInventory() {
-		// TODO Auto-generated method stub
-		return 0;
+	public int getSizeInventory() 
+	{
+		return 3;
 	}
 
 	@Override
-	public ItemStack getStackInSlot(int index) {
-		// TODO Auto-generated method stub
-		return null;
+	public ItemStack getStackInSlot(int index) 
+	{
+		if (index < 0 || index >= this.getSizeInventory())
+	        return null;
+	    return this.inventory[index];
 	}
 
 	@Override
-	public ItemStack decrStackSize(int index, int count) {
-		// TODO Auto-generated method stub
-		return null;
+	public ItemStack decrStackSize(int index, int count) 
+	{
+		if (this.getStackInSlot(index) != null) {
+	        ItemStack itemstack;
+
+	        if (this.getStackInSlot(index).stackSize <= count) {
+	            itemstack = this.getStackInSlot(index);
+	            this.setInventorySlotContents(index, null);
+	            this.markDirty();
+	            return itemstack;
+	        } else {
+	            itemstack = this.getStackInSlot(index).splitStack(count);
+
+	            if (this.getStackInSlot(index).stackSize <= 0) {
+	                this.setInventorySlotContents(index, null);
+	            } else {
+	                //Just to show that changes happened
+	                this.setInventorySlotContents(index, this.getStackInSlot(index));
+	            }
+
+	            this.markDirty();
+	            return itemstack;
+	        }
+	    } else {
+	        return null;
+	    }
 	}
 
 	@Override
-	public ItemStack removeStackFromSlot(int index) {
-		// TODO Auto-generated method stub
-		return null;
+	public ItemStack removeStackFromSlot(int index) 
+	{
+		ItemStack stack = this.getStackInSlot(index);
+	    this.setInventorySlotContents(index, null);
+	    return stack;
+
 	}
 
 	@Override
-	public void setInventorySlotContents(int index, ItemStack stack) {
-		// TODO Auto-generated method stub
-		
+	public void setInventorySlotContents(int index, ItemStack stack) 
+	{
+		if (index < 0 || index >= this.getSizeInventory())
+	        return;
+
+	    if (stack != null && stack.stackSize > this.getInventoryStackLimit())
+	        stack.stackSize = this.getInventoryStackLimit();
+	        
+	    if (stack != null && stack.stackSize == 0)
+	        stack = null;
+
+	    this.inventory[index] = stack;
+	    this.markDirty();	
 	}
 
 	@Override
